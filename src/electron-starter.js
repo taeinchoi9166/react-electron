21da23b847984013094ce89efdab89b85f119dd3
taein <taein1009@naver.com>
2019-06-17 21:22:16 +0900

    first reactron ok!

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,11 +1,12 @@
 const {app, BrowserWindow} = require('electron');
-
+const path = require('path');
 let win;
 
 function createWindow (){
     //create teh browser window
     win = new BrowserWindow({width:800,height:600});
-    const startUrl = process.env.ELECTRON_START_URL;
+    const startUrl = process.env.ELECTRON_START_URL || `file://${path.join(__dirname,'../build/index.html')}`;
+    //해당 url로 파일 가져와 붙여주기
     win.loadURL(startUrl);
 
     //load index.html
@@ -19,7 +20,7 @@ function createWindow (){
     });
 }
 
-//be called when electron has finished
+//electron이 준비되면 윈도우 만들어 띄워주기
 app.on('ready',createWindow);
 
 // called when all windows are closed
--- src/electron-wait-react.js
+++ src/electron-wait-react.js
@@ -1,10 +1,13 @@
+//build되면 여기는 사용 안됨
+
 const net = require('net');
-const port = process.env.PORT ? (process.env.PORT - 100) : 3000;
+const port = process.env.PORT ? (process.env.PORT - 100) : 3000; //포트설정
 
 process.env.ELECTRON_START_URL = `http://localhost:${port}`;
 
 const client = new net.Socket();
 
+//react 구동 성공시 electron을 켜준다.
 let startedElectron = false;
 const tryConnection = () => client.connect({port:port},()=>{
    client.end();
@@ -18,6 +21,6 @@ const tryConnection = () => client.connect({port:port},()=>{
 
 tryConnection();
 
-client.on('error',(error)=>{
+client.on('error',(error)=>{ //안되면 될때까지
    setTimeout(tryConnection,1000);
 });
